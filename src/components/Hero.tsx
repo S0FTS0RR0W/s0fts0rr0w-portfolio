9a5ea0059727245ce55ec31e44181019419bd25c
S0FTS0RR0W <chrlsbrwn4gt@gmail.com>
2025-11-19 20:27:34 +0000

    fixed burger menu and errors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,24 @@ const Hero = () => {
   return (
     <div className="hero-section" id="home">
       <Container className="text-center">
-        <h1 className="display-3">Charlie Brown</h1>
+        <h1 className="display-3 animated-name">
+          {'S0FTS0RR0W'.split(' ').map((word, wordIndex) => (
+            <React.Fragment key={wordIndex}>
+              {word.split('').map((char, charIndex) => (
+                <span
+                  key={`${wordIndex}-${charIndex}`}
+                  className="animated-letter"
+                  style={{ animationDelay: `${(wordIndex * (word.length + 1) + charIndex) * 0.1}s` }}
+                >
+                  {char}
+                </span>
+              ))}
+              {wordIndex < 'S0FTS0RR0W'.split(' ').length - 1 && (
+                <span className="animated-letter" style={{ animationDelay: `${(wordIndex * (word.length + 1) + word.length) * 0.1}s` }}> </span>
+              )}
+            </React.Fragment>
+          ))}
+        </h1>
         <p className="lead">Full-Stack Developer | React Enthusiast | TypeScript Fan</p>
         <Button variant="primary" size="lg" href="#projects">
           View My Work
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './NavigationBar.css';
 
-const NavigationBar = () => {
+const TestBurgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false); // Close menu on link click
+  };
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-      <Container>
-        <Navbar.Brand as={Link} to="/">Charlie Brown</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="test-navbar">
+      <div className="test-navbar-container">
+        <Link to="/" className="test-navbar-brand" onClick={handleLinkClick}>
+          S0FTS0RR0W
+        </Link>
+        <button className="test-burger-button" onClick={toggleMenu} aria-expanded={isOpen}>
+          <div className={`test-burger-line ${isOpen ? 'open' : ''}`}></div>
+          <div className={`test-burger-line ${isOpen ? 'open' : ''}`}></div>
+          <div className={`test-burger-line ${isOpen ? 'open' : ''}`}></div>
+        </button>
+        <div className={`test-menu ${isOpen ? 'open' : ''}`}>
+          <ul className="test-nav-list">
+            <li><Link to="/" onClick={handleLinkClick}>Home</Link></li>
+            <li><Link to="/about" onClick={handleLinkClick}>About</Link></li>
+            <li><Link to="/projects" onClick={handleLinkClick}>Projects</Link></li>
+            <li><Link to="/contact" onClick={handleLinkClick}>Contact</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
-export default NavigationBar;
+export default TestBurgerMenu;
