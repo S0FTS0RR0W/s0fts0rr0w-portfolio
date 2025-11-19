be8a054a15540b92ef298ac4aa89c8af10561de3
S0FTS0RR0W <chrlsbrwn4gt@gmail.com>
2025-11-19 18:49:12 +0000

    updated names

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,13 +11,13 @@ const Footer = () => {
       <Container className="text-center">
         <Row>
           <Col>
-            <a href="#" className="text-white me-3"><GithubIcon size={30} /></a>
-            <a href="#" className="text-white"><LinkedinIcon size={30} /></a>
+            <a href="https://github.com/S0FTS0RR0W" className="text-white me-3"><GithubIcon size={30} /></a>
+            <a href="testicles" className="text-white"><LinkedinIcon size={30} /></a>
           </Col>
         </Row>
         <Row className="mt-3">
           <Col>
-            <p>&copy; {new Date().getFullYear()} Your Name. All Rights Reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Charlie Brown. All Rights Reserved.</p>
           </Col>
         </Row>
       </Container>
