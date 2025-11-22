7433edfc84eea2ef3f53578812492634967bf505
Charlie <chrlsbrwn4gt@gmail.com>
2025-11-22 12:29:01 +0000

    feat: added gradients to buttons to clean up styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
       <Container className="text-center">
         <h1 className={`display-3 typewriter-text ${isPaused ? 'blinking-cursor' : ''}`}>{displayedText}</h1>
         <p className="lead">Full-Stack Developer | React Enthusiast | TypeScript Fan</p>
-        <Button variant="primary" size="lg" href="projects">
+        <Button className="btn" variant="primary" size="lg" href="projects">
           View My Work
         </Button>
       </Container>
