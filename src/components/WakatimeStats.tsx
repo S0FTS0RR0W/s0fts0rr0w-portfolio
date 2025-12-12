4db47f4c8a2b2d167fbacb1218066dbdbb0c8596
S0FTS0RR0W <chrlsbrwn4gt@gmail.com>
2025-12-12 13:17:01 +0000

    Feat: updated wakatime stats chart

--- src/components/WakatimeStats.tsx
+++ src/components/WakatimeStats.tsx
@@ -10,7 +10,7 @@ const WakatimeStats = () => {
         <Row className="justify-content-center">
           <Col md={10} lg={8}>
             <figure>
-              <embed src="https://wakatime.com/share/@3187ed87-8a39-4e81-856d-abf72fd548a7/4a7310c2-4d40-4e4e-8bfc-07c7c384388d.svg"></embed>
+              <embed src="https://wakatime.com/share/@3187ed87-8a39-4e81-856d-abf72fd548a7/76350a5b-1ad4-47f1-8282-6b15aa2fac99.svg"></embed>
             </figure>
           </Col>
         </Row>
